5182749ace53db8bf66cf33c0f608642ad86a97a
elianismedina <elianismedina05@outlook.com>
2024-08-20 07:55:56 -0500

    added 5 boxes inside constraint layoout

--- app/src/main/java/com/elianisdev/constlayputjpc/MainActivity.kt
+++ app/src/main/java/com/elianisdev/constlayputjpc/MainActivity.kt
@@ -4,13 +4,18 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.constraintlayout.compose.ConstraintLayout
 import com.elianisdev.constlayputjpc.ui.theme.ConstLayputJPCTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,28 +25,29 @@ class MainActivity : ComponentActivity() {
         setContent {
             ConstLayputJPCTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
+                    BoxConstraint(
                         modifier = Modifier.padding(innerPadding)
                     )
+
                 }
             }
         }
     }
 }
-
+@Preview
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+fun BoxConstraint(
+    modifier: Modifier = Modifier
+){
+    ConstraintLayout(Modifier.fillMaxSize()) {
+        val (boxRed, boxBlue, boxGreen, boxMagenta, boxYellow) = createRefs()
+        Box(modifier = Modifier.size(100.dp).background(Color.Red).constrainAs(boxRed){})
+        Box(modifier = Modifier.size(100.dp).background(Color.Blue).constrainAs(boxBlue){})
+        Box(modifier = Modifier.size(100.dp).background(Color.Green).constrainAs(boxGreen){})
+        Box(modifier = Modifier.size(100.dp).background(Color.Magenta).constrainAs(boxMagenta){})
+        Box(modifier = Modifier.size(100.dp).background(Color.Yellow).constrainAs(boxYellow){})
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    ConstLayputJPCTheme {
-        Greeting("Android")
     }
+
 }
+
